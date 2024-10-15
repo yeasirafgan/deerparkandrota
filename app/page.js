09d7f566b2a93556f180c5cdf0276d527dbd5bad
yeasirafgan <94146693+yeasirafgan@users.noreply.github.com>
2024-10-15 23:35:25 +0100

    Update home page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 export const metadata = {
   title: 'Deerpark timesheet',
-  description: 'Simple timesheet app for Deerpark staffs...',
+  description: 'Simple timesheet for Deerpark staffs...',
 };
 
 const Home = () => {
@@ -10,10 +10,10 @@ const Home = () => {
     <main className='flex flex-col items-center justify-center min-h-[82vh] w-full px-4 bg-gradient-to-r from-gray-50 to-gray-100'>
       <div className='text-center max-w-2xl'>
         <h1 className='text-4xl md:text-5xl font-extrabold text-gray-700 mb-4'>
-          Deer Park Timesheet App
+          Deer Park Timesheet
         </h1>
         <p className='text-lg md:text-xl text-slate-700 font-semibold'>
-          Please submit your daily worktime...
+          Please submit your hours...
         </p>
       </div>
     </main>
