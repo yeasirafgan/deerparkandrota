8a0ae2b59a09f264c87d8c25f52b6ac1c0e353b2
yeasirafgan <94146693+yeasirafgan@users.noreply.github.com>
2024-10-15 23:52:13 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-// // mainfolder/components/Footer.js
+// mainfolder/components/Footer.js
 
 'use client';
 
@@ -8,14 +8,14 @@ const Footer = () => {
   const { user, isAuthenticated } = useKindeBrowserClient();
 
   return (
-    <footer className='mt-auto text-zinc-400 py-5 px-7 border-t flex flex-col md:flex-row md:justify-between items-center'>
+    <footer className='mt-auto bg-slate-100 text-zinc-500 px-8 py-6 border-t border-zinc-300 flex flex-col md:flex-row md:justify-between items-center'>
       {isAuthenticated && (
-        <p className='text-xs md:text-sm mb-2 md:mb-0 text-center md:text-left'>
-          Logged in as {user?.email}
+        <p className='text-xs md:text-sm mb-3 md:mb-0 text-center md:text-left'>
+          Logged in as <span className='text-slate-700'>{user?.email}</span>
         </p>
       )}
       <small className='text-center md:text-right text-xs md:text-sm'>
-        All Rights Reserved &copy; Deerpark | 2024
+      All Rights Reserved &copy; Deerpark | 2024
       </small>
     </footer>
   );
